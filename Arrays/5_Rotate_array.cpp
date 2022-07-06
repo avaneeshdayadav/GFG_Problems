2356ed534ae7d3f10d623b378aa3f6ff8c9ae90c
avaneeshdayadav <avaneeshdyadav@gmail.com>
2022-07-06 23:21:50 +0530

    Rotate Array

---
+++ Arrays/5_Rotate_array.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    void rotateArr(int arr[], int d, int n){
+        // code here
+        if(d >= n)
+            d = d % n;
+            
+        int l = 0, h = n-1;
+        revArr(arr, l, h);
+        revArr(arr,0,n-d-1);
+        revArr(arr,n-d,n-1);
+    }
+    
+    void revArr(int arr[], int l, int h)
+    {
+        while(l <= h)
+        {
+            swap(arr[l],arr[h]);
+            l++;
+            h--;
+        }
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	//taking testcases
+	cin >> t;
+	
+	while(t--){
+	    int n, d;
+	    
+	    //input n and d
+	    cin >> n >> d;
+	    
+	    int arr[n];
+	    
+	    //inserting elements in the array
+	    for(int i = 0; i < n; i++){
+	        cin >> arr[i];
+	    }
+	    Solution ob;
+	    //calling rotateArr() function
+	    ob.rotateArr(arr, d,n);
+	    
+	    //printing the elements of the array
+	    for(int i =0;i<n;i++){
+	        cout << arr[i] << " ";
+	    }
+	    cout << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
