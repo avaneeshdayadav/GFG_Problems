ad88f43dc791240de4f03d8aa4c7143e6e3d1c6d
avaneeshdayadav <avaneeshdyadav@gmail.com>
2022-07-20 01:44:12 +0530

    Vertical Sum

--- Trees/6_Vertical_sum.cpp
+++ Trees/6_Vertical_sum.cpp
@@ -3,42 +3,69 @@ class Solution{
   public:
     vector <int> verticalSum(Node *root) {
         /// {hor_dist_from_root, node val}
-        map<int,vector<int>> mp;
         vector<int> ans;
         
-        // {hor_dist_from_root, node}
-        queue<pair<int,Node *>> q;
-        q.push({0,root});
         
-        while(!q.empty())
-        {
-            pair<int,Node*> f = q.front();
+        // Queue Soln i.e BFS.
+            // map<int,vector<int>> mp;
+
+            // // {hor_dist_from_root, node}
+            // queue<pair<int,Node *>> q;
+            // q.push({0,root});
             
-            mp[f.first].push_back(f.second->data);
+            // while(!q.empty())
+            // {
+            //     pair<int,Node*> f = q.front();
+                
+            //     mp[f.first].push_back(f.second->data);
+                
+            //     q.pop();
+                
+            //     // If travelling left decrease horizontol dist of left node by 1 from curr node & then push in queue.
+            //     if(f.second->left)
+            //         q.push({f.first - 1,f.second->left});
+                
+            //     // If travelling right increase horizontol dist of right node by 1 from curr node &  then push in queue.
+            //     if(f.second->right)
+            //         q.push({f.first + 1,f.second->right});
+            // }
             
-            q.pop();
+            // for(auto it : mp)
+            // {
+            //     int sum = 0;
+            //     for(auto it2: it.second)
+            //     {
+            //         sum += it2;
+            //     }
+            //     ans.push_back(sum);
+            // }
             
-            // If travelling left decrease horizontol dist of left node by 1 from curr node & then push in queue.
-            if(f.second->left)
-                q.push({f.first - 1,f.second->left});
-            
-            // If travelling right increase horizontol dist of right node by 1 from curr node &  then push in queue.
-            if(f.second->right)
-                q.push({f.first + 1,f.second->right});
-        }
-        
+            // return ans;
+
+        // Inorder trav soln.
+        map < int, int> mp;
+        verticalSumUtil(root,0,mp);
         for(auto it : mp)
         {
-            int sum = 0;
-            for(auto it2: it.second)
-            {
-                sum += it2;
-            }
-            ans.push_back(sum);
+            ans.push_back(it.second);
         }
-        
         return ans;
-        
+    }
+    
+    void verticalSumUtil(Node *node, int hd, map<int, int> &Map)
+    {
+        // Base case
+        if (node == NULL) return;
+      
+        // Recur for left subtree
+        verticalSumUtil(node->left, hd-1, Map);
+      
+        // Add val of current node to
+        // map entry of corresponding hd
+        Map[hd] += node->data;
+      
+        // Recur for right subtree
+        verticalSumUtil(node->right, hd+1, Map);
     }
 };
 
